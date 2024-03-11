625866db4b6d34c4102c1b226d167b910e139b05
Felipe Avila <whiteavila6@gmail.com>
2024-03-11 14:16:35 -0300

    Criação do projeto e branch main

---
+++ src/main/java/com/project/bullcare/BullCareApplication.java
@@ -0,0 +1,13 @@
+package com.project.bullcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/bullcare/BullCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bullcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
