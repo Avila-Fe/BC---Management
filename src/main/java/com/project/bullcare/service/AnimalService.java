5ff7a5e1f10b2c1cde5285df2867f5c144644753
Felipe Avila <whiteavila6@gmail.com>
2024-03-20 19:34:00 -0300

    Criação das constantes e ajuste no UserController

--- src/main/java/com/project/bullcare/service/AnimalService.java
+++ src/main/java/com/project/bullcare/service/AnimalService.java
@@ -7,6 +7,8 @@ import com.project.bullcare.repository.AnimalRepository;
 import com.project.bullcare.util.ParseAnimalDTO;
 import org.springframework.stereotype.Service;
 
+import static com.project.bullcare.util.Constantes.*;
+
 @Service
 public class AnimalService {
 
@@ -18,17 +20,17 @@ public class AnimalService {
         if (animalDTO != null){
             AnimalModel animalModel = parseAnimalDTO.parse(animalDTO);
             animalRepository.save(animalModel);
-            return new ResponseDTO("Concluido","Animal adicionado com sucesso!");
+            return new ResponseDTO(CONCLUIDO,ANIMAL_ADICIONADO);
         }
 
-        return new ResponseDTO("Erro", "Erro ao adicionar animal","Os campos não podem ser vazios!");
+        return new ResponseDTO(ERRO,ANIMAL_NAO_ADICIONADO,CAMPOS_VAZIOS);
     }
 
     public ResponseDTO pesquisaAnimal(String identificacao){
         if (identificacao != null){
            AnimalDTO animal = animalRepository.findByIdentificacao(identificacao);
-            return new ResponseDTO("Concluido", animal.toString());
+            return new ResponseDTO(CONCLUIDO, animal.toString());
         }
-        return new ResponseDTO("Erro", "Erro ao pesquisar animal","O campo identificacão não pode ser vazio!");
+            return new ResponseDTO(ERRO,ANIMAL_NAO_ENCONTRADO,CAMPO_IDENTIFICACAO_VAZIO);
     }
 }
---
+++ src/main/java/com/project/bullcare/util/Constantes.java
@@ -0,0 +1,14 @@
+package com.project.bullcare.util;
+
+public class Constantes {
+
+    public static String ERRO = "Erro";
+    public static String CONCLUIDO = "Concluido";
+    public static String CAMPOS_VAZIOS = "Os campos não podem ser vazios!";
+    public static String CAMPO_IDENTIFICACAO_VAZIO = "O campo identificacão não pode ser vazio!";
+
+
+    public static String ANIMAL_ADICIONADO = "Animal adicionado com sucesso!";
+    public static String ANIMAL_NAO_ADICIONADO = "Erro ao adicionar animal";
+    public static String ANIMAL_NAO_ENCONTRADO = "Erro ao pesquisar animal";
+}
