4f71c369fbb1e3323b9807f1f193dcf9f2105eb9
Guilherme Alves <guilhermeand21@gmail.com>
2024-03-25 15:37:27 -0300

    Correcoes de pesquisa

--- src/main/java/com/project/bullcare/controller/AnimalController.java
+++ src/main/java/com/project/bullcare/controller/AnimalController.java
@@ -1,20 +1,22 @@
 package com.project.bullcare.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.project.bullcare.domain.dto.AnimalDTO;
 import com.project.bullcare.domain.dto.ResponseDTO;
 import com.project.bullcare.repository.AnimalRepository;
 import com.project.bullcare.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/animal")
 public class AnimalController {
-
+    @Autowired
     AnimalService animalService;
 
     @GetMapping("/consultar/{identificacao}")
-    public ResponseEntity<ResponseDTO> consultaAnimal(@PathVariable(name = "identificacao") String identificacao) {
+    public ResponseEntity<ResponseDTO> consultaAnimal(@PathVariable(name = "identificacao") String identificacao) throws JsonProcessingException {
         ResponseDTO responseDTO = animalService.pesquisaAnimal(identificacao);
         return ResponseEntity.ok(responseDTO);
     }
--- src/main/java/com/project/bullcare/service/AnimalService.java
+++ src/main/java/com/project/bullcare/service/AnimalService.java
@@ -1,36 +1,42 @@
 package com.project.bullcare.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.bullcare.domain.dto.AnimalDTO;
 import com.project.bullcare.domain.dto.ResponseDTO;
 import com.project.bullcare.model.AnimalModel;
 import com.project.bullcare.repository.AnimalRepository;
 import com.project.bullcare.util.ParseAnimalDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.project.bullcare.util.Constantes.*;
 
 @Service
 public class AnimalService {
-
+    @Autowired
     AnimalRepository animalRepository;
+    @Autowired
     ParseAnimalDTO parseAnimalDTO;
 
-    public ResponseDTO verificaDados(AnimalDTO animalDTO){
+    public ResponseDTO verificaDados(AnimalDTO animalDTO) {
 
-        if (animalDTO != null){
+        if (animalDTO != null) {
             AnimalModel animalModel = parseAnimalDTO.parse(animalDTO);
             animalRepository.save(animalModel);
-            return new ResponseDTO(CONCLUIDO,ANIMAL_ADICIONADO);
+            return new ResponseDTO(CONCLUIDO, ANIMAL_ADICIONADO);
         }
 
-        return new ResponseDTO(ERRO,ANIMAL_NAO_ADICIONADO,CAMPOS_VAZIOS);
+        return new ResponseDTO(ERRO, ANIMAL_NAO_ADICIONADO, CAMPOS_VAZIOS);
     }
 
-    public ResponseDTO pesquisaAnimal(String identificacao){
-        if (identificacao != null){
-           AnimalDTO animal = animalRepository.findByIdentificacao(identificacao);
-            return new ResponseDTO(CONCLUIDO, animal.toString());
+    public ResponseDTO pesquisaAnimal(String identificacao) throws JsonProcessingException {
+        if (identificacao != null) {
+            AnimalModel animal = animalRepository.findByIdentificacao(identificacao);
+            return new ResponseDTO(CONCLUIDO, animal);
         }
-            return new ResponseDTO(ERRO,ANIMAL_NAO_ENCONTRADO,CAMPO_IDENTIFICACAO_VAZIO);
+        return new ResponseDTO(ERRO, ANIMAL_NAO_ENCONTRADO, CAMPO_IDENTIFICACAO_VAZIO);
     }
 }
