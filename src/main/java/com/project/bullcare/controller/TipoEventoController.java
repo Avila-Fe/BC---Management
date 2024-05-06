98d8860f519673ec5a23af11136a363d5d29bda2
Felipe Avila <whiteavila6@gmail.com>
2024-05-06 16:53:27 -0300

    Ajuste no TipoEventoController.

--- src/main/java/com/project/bullcare/controller/TipoEventoController.java
+++ src/main/java/com/project/bullcare/controller/TipoEventoController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/bc/tipEvento")
+@RequestMapping("/bc/tipoEvento")
 public class TipoEventoController {
 
     @Autowired
